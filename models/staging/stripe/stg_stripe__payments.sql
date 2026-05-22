9e74dda784791085cc11e404e098d7febfe020e2
Xavier Miquel <xmiquel@gmail.com>
2026-05-22 18:42:44 +0200

    Added sources and freshness checks for sources... pero no va bien, ya que si el source freshness casca, el modelo se carga/crea igual... serían procesos separados... mirar freshness, luego ejecutar modelos

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        -- `amount` is currently stored in cents, so we convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+
+    from {{ source ('stripe', 'payment') }}
