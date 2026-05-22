2a08ce60d6ea7099837edf7101cc4dd8fd4f4a31
Xavier Miquel <xmiquel@gmail.com>
2026-05-22 22:11:15 +0200

    Añadir tests al proyecto. Se añade también un dummy_test_preview.sql para poder hacer el preview en vscode con el Preview CTE. La idea es que si falla algún test, se copia el código del mismo (en target\compiled\tests (o haciendo click en el failed)) y se pegue aquí para poder hacer el preview y ver directamente lo que falla.

---
+++ models/dummy_test_preview.sql
@@ -0,0 +1,19 @@
+
+with all_values as (
+
+    select
+        status as value_field,
+        count(*) as n_records
+
+    from `analytics`.`dbt_bhipple`.`stg_jaffle_shop__orders`
+    group by status
+
+)
+
+select *
+from all_values
+where value_field not in (
+    'placed','shipped','completed','returned'
+)
+
+
---
+++ tests/assert_stg_stripe__payment_total_positive.sql
@@ -0,0 +1,5 @@
+select order_id,
+       sum(amount) as total_amount
+  from {{ ref('stg_stripe__payments')}}
+ group by order_id
+having sum(amount) < 0
