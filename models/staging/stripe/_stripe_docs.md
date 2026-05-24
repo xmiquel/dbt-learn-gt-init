3b89f6d1151169f89fa4b74871d15ae7b284f2ee
Xavier Miquel <xmiquel@gmail.com>
2026-05-24 23:27:22 +0200

    refactor_example_1 Lo sobreescribo con código que hay en siguiente lección

---
+++ models/staging/stripe/_stripe_docs.md
@@ -0,0 +1,5 @@
+{% docs payment_amount  %}
+
+The payment amount is currently stored in cents, so we convert it to dollars dividing by 100
+
+{% enddocs %}
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,8 +3,10 @@
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        -- amount` is currently stored in cents, so we convert it to dollars
+        -- amount is currently stored in cents, so we convert it to dollars
         amount / 100 as amount,
         created as created_at
 
     from {{ source ('stripe', 'payment') }}
+
+
